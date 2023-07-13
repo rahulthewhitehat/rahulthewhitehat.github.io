a95b0cd2372ba306dcd24007f96edab872a25795
Rahul Babu M P <106467094+rahulthewhitehat@users.noreply.github.com>
2023-07-13 21:59:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const currentImg = document.querySelector("#current");
 const imgs = document.querySelector(".imgs")
+const img = document.querySelectorAll(".imgs img")
 
+img[0].style.opacity = 0.6;
 imgs.addEventListener("click",(event)=>{
+    imgs.forEach((element)=>{
+        element.style.opacity = 1; } )
     currentImg.src = event.target.src;
     currentImg.classList.add("fade-in");
 
     setTimeout(() => {
         currentImg.classList.remove("fade-in")
     }, 500);
-    
+
+    event.target.style.opacity = 0.6;
 })
 
 
